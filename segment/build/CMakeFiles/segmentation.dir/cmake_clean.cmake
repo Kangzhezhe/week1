10b49a33d43cce48c9e6bc27e8f54c0b80363fd5
kangzhehao <kangzhehao@qq.com>
2023-10-15 23:42:12 +0800

    init

---
+++ segment/build/CMakeFiles/segmentation.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/segmentation.dir/src/main.cpp.o"
+  "CMakeFiles/segmentation.dir/src/main.cpp.o.d"
+  "CMakeFiles/segmentation.dir/src/segment.cpp.o"
+  "CMakeFiles/segmentation.dir/src/segment.cpp.o.d"
+  "segmentation"
+  "segmentation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/segmentation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
